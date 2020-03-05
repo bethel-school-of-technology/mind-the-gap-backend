4a260df500323deec0439bab9d32337e6d5bcf90
Josh <jeigsti777@gmail.com>
2020-03-05 00:38:11 -0600

    adding mongodb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+//add mongoDB to the backend
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://MTBDBUser:<password>@mind-the-gap-nhotr.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 //import routes
 const authRoute = require('./routes/auth');
 
@@ -8,3 +18,4 @@ const authRoute = require('./routes/auth');
 app.use('/api/user', authRoute);
 
 app.listen(8000, () => { console.log('Server started!') });
+
