89ef30d285d641d8f357c3e8f1a87ef2449c636c
River Bryant <river.bryant@ibethel.org>
2020-04-13 19:28:59 -0700

    Got async functions to act like a synchronous function.

--- routes/api/results.js
+++ routes/api/results.js
@@ -16,37 +16,74 @@ var Question = mongoose.model('Question');
 //Get responses by assessment id and user id
 router.get('/', async function (req, res) {
 
-    console.log("Got to Route");
+    console.log("run function begin:");
+    const answerOptionArray = [];
 
-        Response.find({ assessment_id: req.body.assessment_id, user_id: req.body.user_id }, function (err, responses) {
-            console.log('assessment id: ');
-            console.log(req.body.assessment_id);
-            console.log("Response Array:");
-            console.log(responses);
+    async function run() {
+        // await buildArray();
+        // await buildResult();
+        // sendResult();
+
+        buildArray().then(() => buildResult(answerOptionArray)).then(() => sendResult(answerOptionArray));
+    }
+
+    function buildArray() {
+        console.log("Start Build Array")
+        return new Promise(resolve => {
+            Response.find({ assessment_id: req.body.assessment_id, user_id: req.body.user_id }, function (err, responses) {
+            // console.log('assessment id: ');
+            // console.log(req.body.assessment_id);
+            // console.log("Response Array:");
+            // console.log(responses);
             responses.forEach(async response  => {
-                console.log("Got a Response:");
-                console.log(response);
-                // let user_response = await Question.findOne({_id: response.question_id, 'answer_options._id': response.answer_option_id});
-                let user_response = await Question.findOne({'answer_options._id': response.answer_option_id});
-                let answerOptionArray = [];
-                console.log("Response Spacer:");
-                console.log(user_response);
-               console.log(user_response.answer_options);
+            // console.log("Got a Response:");
+            // console.log(response);
+            // let user_response = await Question.findOne({_id: response.question_id, 'answer_options._id': response.answer_option_id});
+            let user_response = await Question.findOne({'answer_options._id': response.answer_option_id});
+            // console.log("Response Spacer:");
+            // console.log(user_response);
+            //console.log(user_response.answer_options);
                 user_response.answer_options.forEach( answer_option => {
                     if(answer_option._id == response.answer_option_id) {
-                        console.log(answer_option.answer_bucket);
+                        // console.log(answer_option.answer_bucket);
                         answerOptionArray.push(answer_option.answer_bucket);
+                        console.log("Array build in process");
+                        console.log(answerOptionArray);
                     }  
-                       
-                
-             });
-                res.json(answerOptionArray);
+                });
+                    console.log("array length compare:");
+                    console.log(answerOptionArray.length);
+                    console.log(responses.length);
+                    if (answerOptionArray.length < responses.length) {
+                        console.log("Building Array....");
+                    } else {
+                        console.log("Array Built:");
+                        console.log(answerOptionArray);
+                        resolve(answerOptionArray);
+                    }    
             });
-    });
 
-    console.log("Here is your array End of Route!");
-    console.log(processData);
-    
+        });
+    });
+    }
+
+    function buildResult(answerOptionArray) {
+        return new Promise(resolve => {
+        console.log("Build Result:");
+        console.log(answerOptionArray);
+        //build results here
+        resolve();
+        });
+    }
+
+    function sendResult(answerOptionArray) {
+        console.log("Array Values:");
+        console.log(answerOptionArray);
+        console.log("Here is your array End of Route!");
+        res.json("Got Here!")
+    }  
+
+    run();
 });
 
 
