211225a03809e4e3f6f48bc9c029704800ddd4b0
taylor davis <thecatsmeow584@gmail.com>
2020-04-16 14:47:35 -0500

    comments|notes

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,7 +65,7 @@ router.post('/signup', function(req, res, next) {
       },
       function(err, doc) { 
         if (err) return res.send(500, {error: err});  
-        return res.send(); 
+        //return res.send(); 
     })
   });
 
