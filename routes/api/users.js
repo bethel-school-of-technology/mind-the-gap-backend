03fd6579e034e88913654f2fb5fb1d43f399b0eb
taylor davis <thecatsmeow584@gmail.com>
2020-04-15 21:47:34 -0500

    profile update edits

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,7 +65,7 @@ router.post('/signup', function(req, res, next) {
       },
       function(err, doc) { 
         if (err) return res.send(500, {error: err});  
-        return res.send('Succesfully saved.'); 
+        return res.send(); 
     })
   });
 
